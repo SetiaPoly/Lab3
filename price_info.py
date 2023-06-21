7766459d885aaf2e208fbc05452280c043c7dae2
SetiaPoly <131343408+SetiaPoly@users.noreply.github.com>
2023-06-22 01:44:57 +0800

    Update price_info.py

--- price_info.py
+++ price_info.py
@@ -8,7 +8,9 @@ def total_cost_shopping():
     total_cost = 0
     for key in price_list.keys():
         if key in quantity_list:
-            # complete the implementation below:
+            total_cost += price_list[key]*quantity_list[key]
+
+    return float(total_cost)
 
     print("total cost = ", total_cost)
 
@@ -18,6 +20,8 @@ def cost_of_fruits(fruit, quantity):
         if key == fruit:
             cost = quantity*price_list[key]
             break
+
+    return (cost)
 
     print("cost of ", quantity, fruit, "=", cost)
 
