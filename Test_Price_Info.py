13313ea4905a700aee0d5833151d82d2430bafc3
SetiaPoly <131343408+SetiaPoly@users.noreply.github.com>
2023-06-21 18:43:14 +0800

    Update Test_Price_Info.py

--- Test_Price_Info.py
+++ Test_Price_Info.py
@@ -11,3 +11,19 @@ def test_cost_of_fruits():
     sample = 186.3
 
     assert(value == sample)
+
+
+#import price_info as bob
+
+#def test_total_cost_shopping():
+#    value = bob.total_cost_shopping()
+#    sample = 46.75
+
+#    assert(value == sample)
+
+#def test_cost_of_fruits():
+#    quantity = 69
+#    expected_result = 69*1.20  #can use that to find the value rather than calculating it
+#    value = bob.cost_of_fruits('pineapple', quantity)
+
+#    assert(value == expected_result)
